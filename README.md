93c1d75a80edba81aebc6c9f98024edda828cd9f
junkka <johan.junkka@gmail.com>
2015-04-03 16:20:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+CV 
+====
+
+This repository containes R scripts to render a vita from yaml data. 
+
+[html CV](http://johanjunkka.com/cv/) and [PDF CV](http://johanjunkka.com/cv/cv.pdf)
---
+++ render_cv.R
@@ -0,0 +1,8 @@
+# render_cv.R
+
+render_cv <- function(){
+  source("src/build_yaml.R")
+  library(rmarkdown)
+  render("cv.Rmd", "pdf_cv", output_dir = "output")
+  render("cv.Rmd", "html_cv", "index.html", output_dir = "output")
+}
