a413133eb309785115baa537104a91a324aa2bd3
junkka <johan.junkka@gmail.com>
2017-04-03 07:08:31 +0200

    rename folder

--- render_cv.R
+++ render_cv.R
@@ -1,7 +1,7 @@
 # render_cv.R
 
 render_cv <- function(){
-  source("src/build_yaml.R")
+  source("R/build_yaml.R")
   library(rmarkdown)
   render("cv.Rmd", "pdf_cv", output_dir = "output")
   render("cv.Rmd", "html_cv", "index.html", output_dir = "output")
